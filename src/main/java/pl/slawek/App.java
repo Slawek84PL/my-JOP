639d8afe499f3f11004dfade9690a6bcb07ba3ae
Slawek <slapyt84@gmail.com>
2023-07-28 14:20:54 +0200

    Pierwsza aplikacja

---
+++ src/main/java/pl/slawek/App.java
@@ -0,0 +1,13 @@
+package pl.slawek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
