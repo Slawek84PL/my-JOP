a9ce2986863288e02262d8f9b7c0809766adf535
Slawek <slapyt84@gmail.com>
2023-07-30 12:05:14 +0200

    Sprawdza podane imię przez użytkownika. Odcina od podanego słowa, wszystkie znaki ktore nie są literami. Imię musi być dłuższe niż 1 znak

---
+++ src/main/java/pl/slawek/NameChecker.java
@@ -0,0 +1,71 @@
+package pl.slawek;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class NameChecker
+{
+    private static final String HELLO = "Witaj";
+    private static String name;
+    private static char sex;
+    private static char sexSymbol;
+
+    public static void main( String[] args )
+    {
+        do {
+            getNameForHello();
+        } while (name == null);
+
+        checkGender();
+
+        System.out.printf("%s %s.\n", HELLO, name);
+
+        System.out.printf("Twoje name ma %s znaków\n", name.length());
+
+        for (char letter : name.toCharArray()) {
+            System.out.println(String.valueOf(letter).toUpperCase(Locale.ROOT));
+        }
+    }
+
+    private static void getNameForHello() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imię:");
+        String tempName = scanner.nextLine();
+
+        if (!checkName(tempName) || tempName.isEmpty()) {
+            System.out.printf("%s nie wygląda to na imię człowieka\n", tempName);
+        }
+    }
+
+    private static boolean checkName(String nameForCheck) {
+
+        String[] splitName = nameForCheck.split("[^A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]");
+
+            String tempName = "";
+            for (final String s : splitName) {
+                tempName = tempName.concat(s);
+            }
+
+            if (tempName.length() > 1) {
+                name = tempName;
+                return true;
+            }
+
+        return false;
+    }
+
+    private static void checkGender() {
+        if (name.endsWith("a")) {
+            sex = 75;
+            sexSymbol = 9792;
+        } else {
+            sex = 77;
+            sexSymbol = 9794;
+        }
+    }
+}
