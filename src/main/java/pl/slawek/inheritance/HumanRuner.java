fcefdaf2cf08a0b94db3723aba7f2cd077a4dc8a
Slawek84PL <93817686+Slawek84PL@users.noreply.github.com>
2023-08-07 10:09:13 +0000

    Zmiana po zmianie nazwy klasy Soldier

--- src/main/java/pl/slawek/inheritance/HumanRuner.java
+++ src/main/java/pl/slawek/inheritance/HumanRuner.java
@@ -2,7 +2,7 @@ package pl.slawek.inheritance;
 
 import pl.slawek.inheritance.model.Chef;
 import pl.slawek.inheritance.model.Human;
-import pl.slawek.inheritance.model.Solider;
+import pl.slawek.inheritance.model.Soldier;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +16,21 @@ class HumanRuner {
         human.setAge(40);
         human.setGrowth(185);
 
-        Human solider = new Solider("Kazek", "Zabójca", 44, 175, "Polska");
-        Solider angrySolider = new Solider("Stefan", "Morderca", 43, 185, "Polska");
+        Human soldier = new Soldier("Kazek", "Zabójca", 44, 175, "Polska");
+        Soldier angrySoldier = new Soldier("Stefan", "Morderca", 43, 185, "Polska");
         Human chef = new Chef("Magda", "GesLer", 150, 115);
 
         List<Human> humans = new ArrayList<>();
         humans.add(human);
-        humans.add(solider);
-        humans.add(angrySolider);
+        humans.add(soldier);
+        humans.add(angrySoldier);
         humans.add(chef);
 
         for (Human h: humans) {
             System.out.printf("nazywam się %s %s \n", h.getName(), h.getSurname());
 
-            if (h instanceof Solider) {
-                ((Solider) h).kill();
+            if (h instanceof Soldier) {
+                ((Soldier) h).kill();
                 System.out.println(h.toString());
             }
 
