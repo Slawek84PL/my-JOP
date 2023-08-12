bebc757f0b6ff1cb84fc296beb9e38a9a91f6348
Slawek <slapyt84@gmail.com>
2023-08-12 10:58:42 +0200

    Dodanie słowa 10

--- src/main/java/pl/slawek/slowa/WordsFromJOP.java
+++ src/main/java/pl/slawek/slowa/WordsFromJOP.java
@@ -11,5 +11,6 @@ public class WordsFromJOP {
         words.add(new Word(4, "programistą", 4, 2, "06:43"));
         words.add(new Word(12, "nawykami",3, 3, "00:00"));
         words.add(new Word(1, "Nie", 5, 2, "01:00"));
+        words.add(new Word(10, "ze", 8, 7, "00:33"));
     }
 }
