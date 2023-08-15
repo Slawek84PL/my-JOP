be17bf7bdd094383f427a74b731e6cdd69671af2
Slawek <slapyt84@gmail.com>
2023-08-15 10:32:08 +0200

    Weapon reprezentuje broń żołnierza i ilość amo w magazynku oraz max ilość amo.

---
+++ src/main/java/pl/slawek/model/soldiers/weapon/Weapon.java
@@ -0,0 +1,33 @@
+package pl.slawek.model.soldiers.weapon;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+public class Weapon {
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    private int amo;
+
+    private final int MAX_AMO = 12;
+
+    public Weapon() {
+        amo = MAX_AMO;
+    }
+
+    public void setAmo(int amo) {
+        if (this.getAmo() == amo) {
+            System.out.println("Musisz przełądować");
+        }
+        this.amo -= amo;
+    }
+
+    protected int reload() {
+        return this.amo += MAX_AMO - this.amo;
+    }
+}
