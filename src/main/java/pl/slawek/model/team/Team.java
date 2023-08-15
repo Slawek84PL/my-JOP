3c62c64b9ff59f6f9dea971a5e5e8230a1abbce6
Slawek <slapyt84@gmail.com>
2023-08-15 10:25:25 +0200

    Team przechowuje listę żołnierzy jako ArrayList

--- src/main/java/pl/slawek/model/team/Team.java
+++ src/main/java/pl/slawek/model/team/Team.java
@@ -1,16 +1,18 @@
 package pl.slawek.model.team;
 
+import lombok.Getter;
 import lombok.ToString;
 import pl.slawek.model.soldiers.Soldier;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 @ToString
 public class Team {
     private static int killedEnemies;
 
-    private Set<Soldier> soldiers = new HashSet<>();
+    @Getter
+    private List<Soldier> soldiers = new LinkedList<>();
 
     public void setSoldiers(Soldier soldier) {
         soldiers.add(soldier);
