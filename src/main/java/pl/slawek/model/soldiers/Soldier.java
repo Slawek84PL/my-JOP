9efd9dae5e44ff09f8e2a688187b4609c78d7646
Slawek <slapyt84@gmail.com>
2023-08-15 10:22:31 +0200

    Rozbudowa klasy o Broń i poziom zdrowia

--- src/main/java/pl/slawek/model/soldiers/Soldier.java
+++ src/main/java/pl/slawek/model/soldiers/Soldier.java
@@ -1,14 +1,28 @@
 package pl.slawek.model.soldiers;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import pl.slawek.model.Human;
+import pl.slawek.model.soldiers.weapon.Weapon;
 
+@NoArgsConstructor
+@Setter
 @ToString(onlyExplicitlyIncluded = true, callSuper = true)
 public class Soldier extends Human {
+
     private static int killedEnemies;
+
     private int myKilledEnemies;
+
     @ToString.Include
     private String nationality;
+
+    private int health = 100;
+
+    @Getter
+    private Weapon weapon;
 
     public Soldier(final String name, final String surname, final int age, final int growth, String nationality) {
         super(name, surname, age, growth);
