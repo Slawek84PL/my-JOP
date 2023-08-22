99e8e9f15bbfef84301d4d88659280620ac04f7c
Slawek <slapyt84@gmail.com>
2023-08-22 16:43:46 +0200

    KLasa przechowuje listę żołnierzy

--- src/main/java/pl/slawek/model/team/Team.java
+++ src/main/java/pl/slawek/model/team/Team.java
@@ -1,22 +1,20 @@
 package pl.slawek.model.team;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import pl.slawek.model.soldiers.Soldier;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@Getter
+@Setter
 @ToString
 public class Team {
     private static int killedEnemies;
 
-    @Getter
     private List<Soldier> soldiers = new LinkedList<>();
-
-    public void setSoldiers(Soldier soldier) {
-        soldiers.add(soldier);
-    }
 
     public void setKilledEnemies() {
         killedEnemies++;
