bd95aa0ad17991018437d2e67d4265a4bb4aa679
Slawek <slapyt84@gmail.com>
2023-08-22 16:46:12 +0200

    Po śmierci żołnierz trafia na specjalną listę

---
+++ src/main/java/pl/slawek/model/team/TeamOfTheDead.java
@@ -0,0 +1,12 @@
+package pl.slawek.model.team;
+
+import pl.slawek.model.soldiers.Soldier;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+
+public class TeamOfTheDead {
+
+    public Deque<Soldier> soldiers = new LinkedList<>();
+}
