adc47d9f21043668843d4c1a714746e8a70feace
Slawek <slapyt84@gmail.com>
2023-08-22 16:43:10 +0200

    Klasa buduje drużyne zołnieży i dodaje jeden support i jeden medic

--- src/main/java/pl/slawek/model/soldiers/SoliderBuilder.java
+++ src/main/java/pl/slawek/model/soldiers/SoliderBuilder.java
@@ -3,23 +3,47 @@ package pl.slawek.model.soldiers;
 import com.github.javafaker.Faker;
 import pl.slawek.model.soldiers.weapon.Weapon;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class SoliderBuilder {
 
-    Faker faker = new Faker(new Locale("pl"));
+    Faker faker;
 
-    public Soldier build(String teamName) {
-        Soldier soldier = new Soldier();
+    public List<Soldier> build(String teamName, String locale) {
+        faker = new Faker(new Locale(locale));
+
+        List<Soldier> tempSoldiersList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            tempSoldiersList.add(new Soldier());
+        }
+
+        tempSoldiersList.add(new Medic());
+        tempSoldiersList.add(new Support());
+
+        for (Soldier soldier : tempSoldiersList) {
+            soldier.setName(faker.name().firstName());
+            soldier.setSurname(faker.name().lastName());
+            soldier.setNationality(teamName);
+            soldier.setWeapon(createWeapon());
+            sayHello(soldier);
+        }
+
+        return tempSoldiersList;
+    }
+
+    private Weapon createWeapon() {
         Weapon weapon = new Weapon();
         weapon.setName(faker.funnyName().name());
+        return weapon;
+    }
 
-        soldier.setName(faker.name().firstName());
-        soldier.setSurname(faker.name().lastName());
-        soldier.setNationality(teamName);
-        soldier.setWeapon(weapon);
-
-        System.out.println(soldier);
-        return soldier;
+    private void sayHello(final Soldier soldier) {
+        System.out.printf("%s %s %s zostaje powołany do słuzby.\n",
+                soldier.getClass().getSimpleName(),
+                soldier.getName().toUpperCase(Locale.ROOT),
+                soldier.getSurname().toUpperCase());
     }
 }
