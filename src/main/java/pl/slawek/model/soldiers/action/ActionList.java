b40365b34f5e3f5aa5a4c733e53b9c5af445a39b
Slawek <slapyt84@gmail.com>
2023-08-24 22:01:29 +0200

    Lista Akcji jakie może wykonać żołnierz

---
+++ src/main/java/pl/slawek/model/soldiers/action/ActionList.java
@@ -0,0 +1,20 @@
+package pl.slawek.model.soldiers.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionList {
+    Map<Integer, Action> actionList = new HashMap<>();
+
+    Map<Integer, Action> getActionList() {
+        return actionList;
+    }
+
+    public void addActionToList(String actionName) {
+        int id = this.actionList.size();
+        Action action = new Action();
+        action.setActionName(actionName);
+
+        this.actionList.put(id, action);
+    }
+}
