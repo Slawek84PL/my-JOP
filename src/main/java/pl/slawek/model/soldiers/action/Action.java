385d3346524d3c5c6b5a0b40ab96a54118044aca
Slawek <slapyt84@gmail.com>
2023-08-24 22:00:58 +0200

    Dodanie możliwych akcji do wywołania na żołnierzu

---
+++ src/main/java/pl/slawek/model/soldiers/action/Action.java
@@ -0,0 +1,28 @@
+package pl.slawek.model.soldiers.action;
+
+class Action {
+    String actionName;
+
+    String getActionName() {
+        return actionName;
+    }
+
+    void setActionName(final String actionName) {
+        this.actionName = actionName;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Action action = (Action) o;
+
+        return actionName.equals(action.actionName);
+    }
+
+    @Override
+    public int hashCode() {
+        return actionName.hashCode();
+    }
+}
