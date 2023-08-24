e5e323237ee4eafb507583554a24ec2e8dbf6818
Slawek <slapyt84@gmail.com>
2023-08-24 21:10:10 +0200

    Implementacja PlayerMoveReader

--- src/main/java/pl/slawek/model/war/WarController.java
+++ src/main/java/pl/slawek/model/war/WarController.java
@@ -1,11 +1,14 @@
 package pl.slawek.model.war;
 
+import pl.slawek.model.logic.PlayerMoveReader;
+import pl.slawek.model.logic.exception.WrongNumberException;
 import pl.slawek.model.soldiers.Soldier;
 
 import java.util.List;
 import java.util.Scanner;
 
 class WarController {
+    private PlayerMoveReader playerMoveReader = new PlayerMoveReader();
     private Scanner scanner = new Scanner(System.in);
     private final int CORECT_TO_LIST_INDEX = 1;
     private final int DEFOULT_VALUE = 0;
@@ -20,25 +23,31 @@ class WarController {
         for (int i = 0; i < soldierList.size(); i++) {
             Soldier soldier = soldierList.get(i);
             System.out.printf("%s - %s %s %s zdrowie: %s amunicja %s.\n",
-                    i + 1, soldier.getClass().getSimpleName(), soldier.getName(), soldier.getSurname(),soldier.getHealth(), soldier.getWeapon().getAmo());
+                    i + CORECT_TO_LIST_INDEX, soldier.getClass().getSimpleName(), soldier.getName(), soldier.getSurname(),soldier.getHealth(), soldier.getWeapon().getAmo());
         }
 
         return soldierList.get(getPlayerAnswer(soldierList.size()));
     }
 
     public int getPlayerAnswer(int maxValue) {
-
-        int answer;
         try {
-            answer = Integer.parseInt(scanner.nextLine());
-            if (answer > maxValue || answer <= 0){
-                System.out.println("Podano żołnierza z poza listy. Do walki staje pierwszy na liście.");
-                return DEFOULT_VALUE;
-            }
-        } catch (Exception e) {
-            System.out.println("Podano niepoprawny numer żołnierza. Do walki staje pierwszy na liście.");
-            return DEFOULT_VALUE;
+            return  playerMoveReader.read(maxValue) - CORECT_TO_LIST_INDEX;
+        } catch (WrongNumberException e) {
+            System.err.println(e.getMessage());
         }
-        return answer - CORECT_TO_LIST_INDEX;
+        System.out.println("Zastosowany zostaje wybór pierwszy z listy.");
+        return DEFOULT_VALUE;
+//        int answer;
+//        try {
+//            answer = Integer.parseInt(scanner.nextLine());
+//            if (answer > maxValue || answer <= 0){
+//                System.out.println("Podano żołnierza z poza listy. Do walki staje pierwszy na liście.");
+//                return DEFOULT_VALUE;
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Podano niepoprawny numer żołnierza. Do walki staje pierwszy na liście.");
+//            return DEFOULT_VALUE;
+//        }
+//        return answer - CORECT_TO_LIST_INDEX;
     }
 }
